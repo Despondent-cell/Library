2936c92bd51e45756cb4d35bff300d4120913875
Despondent-cell <60875434+Despondent-cell@users.noreply.github.com>
2023-11-13 14:51:59 +0100

    131123.4

--- src/pl/javastart/library/app/Library.java
+++ src/pl/javastart/library/app/Library.java
@@ -1,6 +1,10 @@
+package pl.javastart.library.app;
+
+import pl.javastart.library.model.Book;
+
 public class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.4";
+        final String appName = "Biblioteka v0.5";
         Book book1 = new Book("W pustyni i w puszczy","Henryk Sienkiewicz",2010,296,"Greg","9712924912812");
         Book book2 = new Book("Java. Efektywne programowanie. Wydanie II", "Joshua Bloch", 2009, 352, "Helion",
                 "9788324620845");
