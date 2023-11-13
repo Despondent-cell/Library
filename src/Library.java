083d8e8cc1e5e93a6f91f48aaeec6325067a2a13
Despondent-cell <60875434+Despondent-cell@users.noreply.github.com>
2023-11-13 14:34:15 +0100

    131123.3

--- src/Library.java
+++ src/Library.java
@@ -1,6 +1,6 @@
 public class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.3";
+        final String appName = "Biblioteka v0.4";
         Book book1 = new Book("W pustyni i w puszczy","Henryk Sienkiewicz",2010,296,"Greg","9712924912812");
         Book book2 = new Book("Java. Efektywne programowanie. Wydanie II", "Joshua Bloch", 2009, 352, "Helion",
                 "9788324620845");
