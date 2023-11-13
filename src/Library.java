81392d6b5f1fbfbd1c7287667985f6775429f167
Despondent-cell <60875434+Despondent-cell@users.noreply.github.com>
2023-11-13 13:30:29 +0100

    Fist commit Library

--- src/Library.java
+++ src/Library.java
@@ -1,21 +1,22 @@
 public class Library {
     public static void main(String[] args) {
         final String appName = "Biblioteka v0.1";
-        String title = "W pustyni i w puszczy";
-        String author = "Henryk Sienkiewicz";
-        int releaseDate =  2010;
-        int pages=296;
-        String publisher = "Greg";
-        String isbn = "9712924912812";
+        Book Book1 = new Book();
+        Book1.title = "W pustyni i w puszczy";
+        Book1.author = "Henryk Sienkiewicz";
+        Book1.releaseDate =  2010;
+        Book1.pages=296;
+        Book1.publisher = "Greg";
+        Book1.isbn = "9712924912812";
 
         System.out.println(appName);
         System.out.println("Książki dostepne w bibliotece");
-        System.out.println(title);
-        System.out.println(author);
-        System.out.println(releaseDate);
-        System.out.println(pages);
-        System.out.println(publisher);
-        System.out.println(isbn);
+        System.out.println(Book1.title);
+        System.out.println(Book1.author);
+        System.out.println(Book1.releaseDate);
+        System.out.println(Book1.pages);
+        System.out.println(Book1.publisher);
+        System.out.println(Book1.isbn);
 
     }
 
