a98f655b5f3bf7215ab0ca9cc3e672a464a1ccec
Despondent-cell <60875434+Despondent-cell@users.noreply.github.com>
2023-11-13 14:17:45 +0100

    131123.1

--- src/Library.java
+++ src/Library.java
@@ -1,22 +1,17 @@
 public class Library {
     public static void main(String[] args) {
         final String appName = "Biblioteka v0.1";
-        Book Book1 = new Book();
-        Book1.title = "W pustyni i w puszczy";
-        Book1.author = "Henryk Sienkiewicz";
-        Book1.releaseDate =  2010;
-        Book1.pages=296;
-        Book1.publisher = "Greg";
-        Book1.isbn = "9712924912812";
+        Book book1 = new Book("W pustyni i w puszczy","Henryk Sienkiewicz",2010,296,"Greg","9712924912812");
+        Book book2 = new Book("Java. Efektywne programowanie. Wydanie II", "Joshua Bloch", 2009, 352, "Helion",
+                "9788324620845");
+        Book book3 = new Book("SCJP Sun Certified Programmer for Java 6 Study Guide", "Bert Bates, Katherine Sierra",
+                2008, 851, "McGraw-Hill Osborne Media", "9780071591065");
 
-        System.out.println(appName);
-        System.out.println("Książki dostepne w bibliotece");
-        System.out.println(Book1.title);
-        System.out.println(Book1.author);
-        System.out.println(Book1.releaseDate);
-        System.out.println(Book1.pages);
-        System.out.println(Book1.publisher);
-        System.out.println(Book1.isbn);
+        System.out.print(appName);
+        System.out.println("Książki dostępne w bibliotece:");
+        book1.printInfo();
+        book2.printInfo();
+        book3.printInfo();
 
     }
 
