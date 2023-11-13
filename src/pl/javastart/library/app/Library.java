6a2bfe43af504d0b6a5025f0f3dc7dd0e19c29c8
Despondent-cell <60875434+Despondent-cell@users.noreply.github.com>
2023-11-13 15:35:08 +0100

    131123.6

--- src/pl/javastart/library/app/Library.java
+++ src/pl/javastart/library/app/Library.java
@@ -1,23 +1,22 @@
 package pl.javastart.library.app;
-
+import pl.javastart.library.io.DataReader;
 import pl.javastart.library.model.Book;
 
 public class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.7";
-        Book books[] = new Book[1000];
-         books[0] = new Book("W pustyni i w puszczy","Henryk Sienkiewicz",2010,296,"Greg","9712924912812");
-         books[1] = new Book("Java. Efektywne programowanie. Wydanie II", "Joshua Bloch", 2009, 352, "Helion",
-                "9788324620845");
-         books[2] = new Book("SCJP Sun Certified Programmer for Java 6 Study Guide", "Bert Bates, Katherine Sierra",
-                2008, 851, "McGraw-Hill Osborne Media", "9780071591065");
+        final String appName = "Biblioteka v0.8";
 
-        System.out.print(appName);
-        System.out.println("Książki dostępne w bibliotece:");
+        Book[] books = new Book[1000];
+        DataReader dataReader = new DataReader();
+
+        System.out.println(appName);
+        System.out.println("Wprowadź nową książkę:");
+        books[0] = dataReader.readAndCreateBook();
+        books[1] = dataReader.readAndCreateBook();
+        dataReader.close();
+
         books[0].printInfo();
         books[1].printInfo();
-        books[2].printInfo();
-
+        System.out.println("System może przechowywać do " + books.length + " książek");
     }
-
 }
