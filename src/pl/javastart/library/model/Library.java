bbe4da8b27c4b2afb6c7d2d58a1df4c049c7d3ff
Despondent-cell <60875434+Despondent-cell@users.noreply.github.com>
2023-11-13 16:21:47 +0100

    131123.7

---
+++ src/pl/javastart/library/model/Library.java
@@ -0,0 +1,24 @@
+package pl.javastart.library.model;
+
+public class Library {
+    private final int maxBooks = 1000;
+    private int booksNumber= 0;
+    Book[] books = new Book[maxBooks];
+    public void addBook(Book book){
+        if(booksNumber<maxBooks){
+            books[booksNumber]=book;
+            booksNumber++;
+        }
+        else {
+            System.out.println("Maksymalna liczba ksiązek została osiągnięta");
+        }
+    }
+    public void printBooks() {
+        if(booksNumber == 0) {
+            System.out.println("Brak książek w bibliotece");
+        }
+        for(int i=0; i<booksNumber; i++) {
+            books[i].printInfo();
+        }
+    }
+}
