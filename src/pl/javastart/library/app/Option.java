a9d0bc521a6f4b03a894693deb765eb33da909ec
Despondent-cell <60875434+Despondent-cell@users.noreply.github.com>
2023-11-14 11:59:51 +0100

    141123.3

---
+++ src/pl/javastart/library/app/Option.java
@@ -0,0 +1,26 @@
+package pl.javastart.library.app;
+
+public enum Option {
+    EXIT(0, "Wyjście z programu"),
+    ADD_BOOK(1, "Dodanie książki"),
+    ADD_MAGAZINE(2,"Dodanie magazynu/gazety"),
+    PRINT_BOOKS(3, "Wyświetlenie dostępnych książek"),
+    PRINT_MAGAZINES(4, "WYświetlenie dostępnych magazynów/gazet");
+
+    private final int value;
+    private final String description;
+
+    Option(int value, String description) {
+        this.value = value;
+        this.description = description;
+    }
+    @Override
+    public String toString() {
+        return value + " - " + description;
+    }
+
+    static Option createFromInt(int option){
+        return Option.values()[option];
+    }
+}
+
